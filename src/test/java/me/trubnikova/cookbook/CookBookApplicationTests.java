26781296d9e8db971a580be3eec95b15691c5be8
eptrubnikova <eptrubnikova@gmail.com>
2023-01-30 19:41:27 +0600

    Initial commit

---
+++ src/test/java/me/trubnikova/cookbook/CookBookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.trubnikova.cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
